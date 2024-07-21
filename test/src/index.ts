7c4d24fffa26c64b0de82bd911edd8325d0a5553
sidhys <sidhys1@gmail.com>
2024-07-21 18:04:17 -0400

    Delete test directory

--- test/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * as CoreUtil from './core.js'
-export * as VMUtil   from './vm/util.js'
-
-export * from './types.js'
-export * from './util.js'
-
--- test/wip/demo/draft/alice/list.ts
+++
@@ -1,9 +0,0 @@
-import { DraftSession } from '@scrow/core'
-import { signer }       from './config.js'
-import { address }      from '../terms.js'
-
-console.log(signer.pubkey)
-
-const drafts = await DraftSession.list(address, signer)
-
-console.dir(drafts, { depth : null })
